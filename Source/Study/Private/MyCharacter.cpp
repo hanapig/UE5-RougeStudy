2157efd7c46a0ee8534ef9bb09187d4137639042
hanapig <1152389162@qq.com>
2024-08-10 13:55:16 +0800

    move

--- Source/Study/Private/MyCharacter.cpp
+++ Source/Study/Private/MyCharacter.cpp
@@ -3,12 +3,19 @@
 
 #include "MyCharacter.h"
 
+
+
 // Sets default values
 AMyCharacter::AMyCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	USc = CreateDefaultSubobject<USpringArmComponent>("USC");
+	USc->SetupAttachment(RootComponent);
+	Uc = CreateDefaultSubobject<UCameraComponent>("Uc");
+	Uc->SetupAttachment(USc);
+	USc->bUsePawnControlRotation = true;
+	Uc->bUsePawnControlRotation = false;
 }
 
 // Called when the game starts or when spawned
@@ -30,5 +37,79 @@ void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if (APlayerController* play = Cast<APlayerController>(GetController()))
+	{
+		if (ULocalPlayer* LocalPlayer = play->GetLocalPlayer())
+		{
+			UEnhancedInputLocalPlayerSubsystem* InputSubsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
+			if (InputSubsystem)
+			{
+				InputSubsystem->AddMappingContext(Map, 0);
+				
+			}
+		}
+	}
+
+	if (Input)
+	{
+		Input->BindAction(W, ETriggerEvent::Triggered, this, &AMyCharacter::MoveW);
+		Input->BindAction(S, ETriggerEvent::Triggered, this, &AMyCharacter::MoveS);
+		Input->BindAction(A, ETriggerEvent::Triggered, this, &AMyCharacter::MoveA);
+		Input->BindAction(D, ETriggerEvent::Triggered, this, &AMyCharacter::MoveD);
+		Input->BindAction(MX, ETriggerEvent::Triggered, this, &AMyCharacter::MoveX);
+		Input->BindAction(MY, ETriggerEvent::Triggered, this, &AMyCharacter::MoveY);
+		UE_LOG(LogTemp, Warning, TEXT("bindcom"));
+	}
 }
+
+void AMyCharacter::MoveW(const FInputActionValue& value)
+{
+	float WValue = value.Get<float>();
+	AddMovementInput(GetActorForwardVector(), WValue);
+}
+
+void AMyCharacter::MoveS(const FInputActionValue& value)
+{
+	float WValue = value.Get<float>();
+	AddMovementInput(GetActorForwardVector(), WValue);
+}
+
+void AMyCharacter::MoveD(const FInputActionValue& value)
+{
+	float WValue = value.Get<float>();
+	AddMovementInput(GetActorRightVector(), WValue);
+}
+
+void AMyCharacter::MoveA(const FInputActionValue& value)
+{
+	float WValue = value.Get<float>();
+	AddMovementInput(GetActorRightVector(), WValue);
+}
+
+void AMyCharacter::MoveX(const FInputActionValue& val)
+{
+	float AxisValue = val.Get<float>();
+	if (AxisValue != 0.f && Controller && Controller->IsLocalPlayerController())
+	{
+		APlayerController* const PC = CastChecked<APlayerController>(Controller);
+		PC->AddYawInput(AxisValue);
+	}
+}
+
+void AMyCharacter::MoveY(const FInputActionValue& val)
+{
+	float PitchValue = val.Get<float>();
+	if (Controller)
+	{
+		APlayerController* PC = Cast<APlayerController>(Controller);
+		if (PC)
+		{
+			PC->AddPitchInput(PitchValue);
+		}
+	}
+}
+
+
 
--- Source/Study/Public/MyCharacter.h
+++ Source/Study/Public/MyCharacter.h
@@ -3,8 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputMappingContext.h"
+#include "GameFramework\SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 #include "GameFramework/Character.h"
 #include "MyCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
 
 UCLASS()
 class STUDY_API AMyCharacter : public ACharacter
@@ -18,12 +26,38 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "C")
+	UCameraComponent* Uc;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "C")
+	USpringArmComponent* USc;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	void MoveW(const FInputActionValue& value);
+	void MoveS(const FInputActionValue& value);
+	void MoveD(const FInputActionValue& value);
+	void MoveA(const FInputActionValue& value);
+	void MoveX(const FInputActionValue& val);
+	void MoveY(const FInputActionValue& val);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	 TObjectPtr<UInputMappingContext>Map;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction>W;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction>S;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction>D;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction>A;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction>MX;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction>MY;
+
+
+
 
 };
